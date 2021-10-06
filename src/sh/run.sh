006b1861a6c435bfba31ef427d6b1f629f4d4183
Hauke Mettendorf <hauke@mettendorf.it>
2021-10-06 18:02:19 +0200

    Fixed quotation in metric

--- src/sh/run.sh
+++ src/sh/run.sh
@@ -74,6 +74,6 @@ mkdir -p "$(dirname ${TARGET_FILE})"
 while true; do
   echo "# HELP extra_labels like hostname of node" > "${TARGET_FILE}"
   echo "# TYPE extra_labels count" >> "${TARGET_FILE}"
-  echo "extra_labels{hostname=$(cat /etc/hostname)} 0" >> "${TARGET_FILE}"
+  echo "extra_labels{hostname=\"$(cat /etc/hostname)\"} 0" >> "${TARGET_FILE}"
   sleep 30
 done
